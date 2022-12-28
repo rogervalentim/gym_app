89ae008711140104776e81ed7727e726cbd3f5ff
rogervalentim <valentim.roger33@gmail.com>
2022-12-28 17:27:36 -0300

    add Benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -28,9 +29,11 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <Home 
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage} 
+      <Home
+        setSelectedPage={setSelectedPage}
+      />
+      <Benefits
+        setSelectedPage={setSelectedPage}
       />
     </div>
   );
