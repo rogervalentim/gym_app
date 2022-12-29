6cf3f51113d823aad794a36b25abe2612063ba3e
rogervalentim <valentim.roger33@gmail.com>
2022-12-28 21:32:22 -0300

    Add Our Classes Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
+import OurClasses from "@/scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -35,6 +36,7 @@ function App() {
       <Benefits
         setSelectedPage={setSelectedPage}
       />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
